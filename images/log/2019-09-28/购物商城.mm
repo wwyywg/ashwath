6fa53d33a9a04a293e7549be61e50e0b4f886561
wangwu <15268131701@163.com>
2019-09-28 15:52:15 +0800

    重大升级2

---
+++ images/log/2019-09-28/购物商城.mm
@@ -0,0 +1,73 @@
+<map>
+		<node ID="root" TEXT="购物商城">		<node TEXT="商品中心 #商品中心" ID="1d216d769363ab189" STYLE="bubble" POSITION="right">
+		<node TEXT="首页推荐商品展示" ID="4b16d7697c61211f" STYLE="fork">
+		</node>
+		<node TEXT="全部商品展示" ID="39f16d76985bea091" STYLE="fork">
+		</node>
+		<node TEXT="商品明细" ID="7016d76988f8704" STYLE="fork">
+		</node>
+		<node TEXT="商品评价" ID="5316d7699894910c" STYLE="fork">
+		</node>
+		<node TEXT="商品收藏" ID="12816d7699bd8c043" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="用户中心" ID="15f16d7693edc10ea" STYLE="bubble" POSITION="right">
+		<node TEXT="登陆/注册" ID="1b516d7699f53908e" STYLE="fork">
+		</node>
+		<node TEXT="验证码" ID="14616d769a640f0b4" STYLE="fork">
+		</node>
+		<node TEXT="用户信息修改" ID="2bb16d769a746d049" STYLE="fork">
+		</node>
+		<node TEXT="用户地址维护" ID="38916d769a926514b" STYLE="fork">
+		</node>
+		<node TEXT="邮箱验证" ID="4016d769abe6b167" STYLE="fork">
+		</node>
+		<node TEXT="登陆功能的优化" ID="18716d769af1f4066" STYLE="fork">
+		</node>
+		<node TEXT="个人资料完善" ID="19d16d769b21a50a1" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="账户中心" ID="ff16d76940d370a3" STYLE="bubble" POSITION="right">
+		<node TEXT="积分" ID="2eb16d769c90190c9" STYLE="fork">
+		</node>
+		<node TEXT="优惠券" ID="2fb16d769ca3040c3" STYLE="fork">
+		</node>
+		<node TEXT="提现/充值" ID="33416d769cdace04d" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="订单" ID="15616d76942515165" STYLE="bubble" POSITION="right">
+		<node TEXT="订单查询" ID="2916d769d2035029" STYLE="fork">
+		</node>
+		<node TEXT="创建订单" ID="25e16d769d3afd112" STYLE="fork">
+		</node>
+		<node TEXT="删除订单" ID="2516d769d4f2b0c6" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="购物车" ID="e316d76944cd7089" STYLE="bubble" POSITION="left">
+		<node TEXT="添加商品到购物车" ID="1b416d769b90710c1" STYLE="fork">
+		</node>
+		<node TEXT="修改购物车" ID="14816d769bc558155" STYLE="fork">
+		</node>
+		<node TEXT="删除购物车" ID="8016d769bf22b162" STYLE="fork">
+		</node>
+		<node TEXT="删除购物车商品" ID="39d16d769c1e710e4" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="支付" ID="2ff16d7694680713a" STYLE="bubble" POSITION="left">
+		<node TEXT="微信支付" ID="3d316d769c40c713b" STYLE="fork">
+		</node>
+		<node TEXT="支付宝支付" ID="15c16d769c6c250da" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="后台运营系统" ID="34c16d7694770809d" STYLE="bubble" POSITION="left">
+		</node>
+		<node TEXT="营销平台" ID="1af16d7694a89404b" STYLE="bubble" POSITION="left">
+		<node TEXT="秒杀" ID="cc16d769d7dd503d" STYLE="fork">
+		</node>
+		<node TEXT="优惠券" ID="31316d769d98dd08f" STYLE="fork">
+		</node>
+		<node TEXT="促销" ID="31316d769db19b17f" STYLE="fork">
+		</node>
+		</node>
+</node>
+</map>
